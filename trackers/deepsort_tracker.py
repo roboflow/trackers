6decb742c7c5eaf47cd207e8e791f06a3fd8b1d0
soumik12345 <19soumik.rakshit96@gmail.com>
2025-04-09 03:39:15 +0530

    update: imports

--- trackers/deepsort_tracker.py
+++ trackers/deepsort_tracker.py
@@ -4,6 +4,7 @@ import torch
 import torchvision.transforms as transforms
 from scipy.spatial.distance import cdist
 from torch.serialization import add_safe_globals
+import cv2
 
 try:
     from siamese_net import SiameseNetwork
