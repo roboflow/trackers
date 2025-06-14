954366e0c0105567ad1c42113942b4d168b92433
soumik12345 <19soumik.rakshit96@gmail.com>
2025-06-13 23:13:01 -0500

    refactor: train + validation batch loops

--- trackers/core/reid/trainers/identity_trainer.py
+++ trackers/core/reid/trainers/identity_trainer.py
@@ -41,22 +41,75 @@ class IdentityTrainer:
         train_logs = {"train/loss": loss.item()}
         return train_logs
 
-    def train(
+    def validation_step(self, image_batch: torch.Tensor, entity_id_batch: torch.Tensor):
+        with torch.inference_mode():
+            outputs = self.model(image_batch)
+            logits = F.log_softmax(outputs, dim=1)
+            loss = F.cross_entropy(
+                logits, entity_id_batch, label_smoothing=self.label_smoothing
+            )
+
+        validation_logs = {"validation/loss": loss.item()}
+        return validation_logs
+
+    def batch_train_loop(
         self,
         train_loader: DataLoader,
+        epoch: int,
         epochs: int,
-        validation_loader: Optional[DataLoader] = None,
-        callbacks: list[BaseCallback] = [],
-        checkpoint_interval: Optional[int] = None,
-        log_dir: str = "logs",
-        config: dict[str, Any] = {},
+        callbacks: list[BaseCallback],
     ):
-        for epoch in tqdm(range(epochs), desc="Training"):
-            accumulated_train_logs: dict[str, Union[float, int]] = {}
+        accumulated_train_logs: dict[str, Union[float, int]] = {}
+        for idx, data in tqdm(
+            enumerate(train_loader),
+            total=len(train_loader),
+            desc=f"Training Epoch {epoch + 1}/{epochs}",
+            leave=False,
+        ):
+            image_batch = data["image"]
+            entity_id_batch = data["entity_id"]
+            if self.train_transforms is not None:
+                image_batch = self.train_transforms(image_batch)
+
+            image_batch = image_batch.to(self.device)
+            entity_id_batch = entity_id_batch.to(self.device)
+
+            if callbacks:
+                for callback in callbacks:
+                    callback.on_train_batch_start({}, epoch * len(train_loader) + idx)
+
+            train_logs = self.train_step(image_batch, entity_id_batch)
+
+            for key, value in train_logs.items():
+                accumulated_train_logs[key] = accumulated_train_logs.get(key, 0) + value
+
+            if callbacks:
+                for callback in callbacks:
+                    for key, value in train_logs.items():
+                        callback.on_train_batch_end(
+                            {f"batch/{key}": value}, epoch * len(train_loader) + idx
+                        )
+
+        for key, value in accumulated_train_logs.items():
+            accumulated_train_logs[key] = value / len(train_loader)
+
+        if callbacks:
+            for callback in callbacks:
+                callback.on_train_epoch_end(accumulated_train_logs, epoch)
+
+    def batch_validation_loop(
+        self,
+        validation_loader: DataLoader,
+        epoch: int,
+        epochs: int,
+        callbacks: list[BaseCallback],
+    ):
+        accumulated_validation_logs: dict[str, Union[float, int]] = {}
+        if validation_loader is not None:
             for idx, data in tqdm(
-                enumerate(train_loader),
-                total=len(train_loader),
-                desc=f"Training Epoch {epoch + 1}/{epochs}",
+                enumerate(validation_loader),
+                total=len(validation_loader),
+                desc=f"Validation Epoch {epoch + 1}/{epochs}",
                 leave=False,
             ):
                 image_batch = data["image"]
@@ -69,30 +122,55 @@ class IdentityTrainer:
 
                 if callbacks:
                     for callback in callbacks:
-                        callback.on_train_batch_start(
-                            {}, epoch * len(train_loader) + idx
+                        callback.on_validation_batch_start(
+                            {}, epoch * len(validation_loader) + idx
                         )
 
-                train_logs = self.train_step(image_batch, entity_id_batch)
+                validation_logs = self.validation_step(image_batch, entity_id_batch)
 
-                for key, value in train_logs.items():
-                    accumulated_train_logs[key] = (
-                        accumulated_train_logs.get(key, 0) + value
+                for key, value in validation_logs.items():
+                    accumulated_validation_logs[key] = (
+                        accumulated_validation_logs.get(key, 0) + value
                     )
 
                 if callbacks:
                     for callback in callbacks:
-                        for key, value in train_logs.items():
-                            callback.on_train_batch_end(
-                                {f"batch/{key}": value}, epoch * len(train_loader) + idx
+                        for key, value in validation_logs.items():
+                            callback.on_validation_batch_end(
+                                {f"batch/{key}": value},
+                                epoch * len(validation_loader) + idx,
                             )
+            for key, value in accumulated_validation_logs.items():
+                accumulated_validation_logs[key] = value / len(validation_loader)
 
-            for key, value in accumulated_train_logs.items():
-                accumulated_train_logs[key] = value / len(train_loader)
+        if callbacks:
+            for callback in callbacks:
+                callback.on_validation_epoch_end(accumulated_validation_logs, epoch)
 
-            if callbacks:
-                for callback in callbacks:
-                    callback.on_train_epoch_end(accumulated_train_logs, epoch)
+    def train(
+        self,
+        train_loader: DataLoader,
+        epochs: int,
+        validation_loader: Optional[DataLoader] = None,
+        callbacks: list[BaseCallback] = [],
+        checkpoint_interval: Optional[int] = None,
+        log_dir: str = "logs",
+        config: dict[str, Any] = {},
+    ):
+        for epoch in tqdm(range(epochs), desc="Training"):
+            self.batch_train_loop(
+                train_loader=train_loader,
+                epoch=epoch,
+                epochs=epochs,
+                callbacks=callbacks,
+            )
+
+            self.batch_validation_loop(
+                validation_loader=validation_loader,
+                epoch=epoch,
+                epochs=epochs,
+                callbacks=callbacks,
+            )
 
             save_checkpoint(
                 model=self.model,
--- trackers/core/reid/trainers/triplets_trainer.py
+++ trackers/core/reid/trainers/triplets_trainer.py
@@ -114,6 +114,130 @@ class TripletsTrainer:
 
         return validation_logs
 
+    def batch_train_loop(
+        self,
+        train_loader: DataLoader,
+        epoch: int,
+        epochs: int,
+        callbacks: list[BaseCallback],
+        metrics_list: Optional[list[TripletMetric]] = None,
+    ):
+        accumulated_train_logs: dict[str, Union[float, int]] = {}
+        for idx, data in tqdm(
+            enumerate(train_loader),
+            total=len(train_loader),
+            desc=f"Training Epoch {epoch + 1}/{epochs}",
+            leave=False,
+        ):
+            anchor_image, positive_image, negative_image = data
+            if self.train_transforms is not None:
+                anchor_image = self.train_transforms(anchor_image)
+                positive_image = self.train_transforms(positive_image)
+                negative_image = self.train_transforms(negative_image)
+
+            anchor_image = anchor_image.to(self.device)
+            positive_image = positive_image.to(self.device)
+            negative_image = negative_image.to(self.device)
+
+            if callbacks:
+                for callback in callbacks:
+                    callback.on_train_batch_start({}, epoch * len(train_loader) + idx)
+
+            train_logs = self.train_step(
+                anchor_image, positive_image, negative_image, metrics_list or []
+            )
+
+            for key, value in train_logs.items():
+                accumulated_train_logs[key] = accumulated_train_logs.get(key, 0) + value
+
+            if callbacks:
+                for callback in callbacks:
+                    for key, value in train_logs.items():
+                        callback.on_train_batch_end(
+                            {f"batch/{key}": value}, epoch * len(train_loader) + idx
+                        )
+
+        for key, value in accumulated_train_logs.items():
+            accumulated_train_logs[key] = value / len(train_loader)
+
+        # Compute and add training metrics to logs
+        if metrics_list is not None:
+            for metric in metrics_list:
+                accumulated_train_logs[f"train/{metric!s}"] = metric.compute()
+        # Metrics are reset at the start of the next epoch or before validation
+
+        if callbacks:
+            for callback in callbacks:
+                callback.on_train_epoch_end(accumulated_train_logs, epoch)
+
+    def batch_validation_loop(
+        self,
+        validation_loader: DataLoader,
+        epoch: int,
+        epochs: int,
+        callbacks: list[BaseCallback],
+        metrics_list: Optional[list[TripletMetric]] = None,
+    ):
+        accumulated_validation_logs: dict[str, Union[float, int]] = {}
+        if validation_loader is not None:
+            # Reset metrics for validation
+            if metrics_list is not None:
+                for metric in metrics_list:
+                    metric.reset()
+            for idx, data in tqdm(
+                enumerate(validation_loader),
+                total=len(validation_loader),
+                desc=f"Validation Epoch {epoch + 1}/{epochs}",
+                leave=False,
+            ):
+                if callbacks:
+                    for callback in callbacks:
+                        callback.on_validation_batch_start(
+                            {}, epoch * len(validation_loader) + idx
+                        )
+
+                anchor_image, positive_image, negative_image = data
+                if self.train_transforms is not None:
+                    anchor_image = self.train_transforms(anchor_image)
+                    positive_image = self.train_transforms(positive_image)
+                    negative_image = self.train_transforms(negative_image)
+
+                anchor_image = anchor_image.to(self.device)
+                positive_image = positive_image.to(self.device)
+                negative_image = negative_image.to(self.device)
+
+                validation_logs = self.validation_step(
+                    anchor_image, positive_image, negative_image, metrics_list or []
+                )
+
+                for key, value in validation_logs.items():
+                    accumulated_validation_logs[key] = (
+                        accumulated_validation_logs.get(key, 0) + value
+                    )
+
+                if callbacks:
+                    for callback in callbacks:
+                        for key, value in validation_logs.items():
+                            callback.on_validation_batch_end(
+                                {f"batch/{key}": value},
+                                epoch * len(validation_loader) + idx,
+                            )
+
+            for key, value in accumulated_validation_logs.items():
+                accumulated_validation_logs[key] = value / len(validation_loader)
+
+            # Compute and add validation metrics to logs
+            if metrics_list is not None:
+                for metric in metrics_list:
+                    accumulated_validation_logs[f"validation/{metric!s}"] = (
+                        metric.compute()
+                    )
+            # Metrics will be reset at the start of the next training epoch loop
+
+        if callbacks:
+            for callback in callbacks:
+                callback.on_validation_epoch_end(accumulated_validation_logs, epoch)
+
     def train(
         self,
         train_loader: DataLoader,
@@ -132,118 +256,22 @@ class TripletsTrainer:
                     metric.reset()
 
             # Training loop over batches
-            accumulated_train_logs: dict[str, Union[float, int]] = {}
-            for idx, data in tqdm(
-                enumerate(train_loader),
-                total=len(train_loader),
-                desc=f"Training Epoch {epoch + 1}/{epochs}",
-                leave=False,
-            ):
-                anchor_image, positive_image, negative_image = data
-                if self.train_transforms is not None:
-                    anchor_image = self.train_transforms(anchor_image)
-                    positive_image = self.train_transforms(positive_image)
-                    negative_image = self.train_transforms(negative_image)
-
-                anchor_image = anchor_image.to(self.device)
-                positive_image = positive_image.to(self.device)
-                negative_image = negative_image.to(self.device)
-
-                if callbacks:
-                    for callback in callbacks:
-                        callback.on_train_batch_start(
-                            {}, epoch * len(train_loader) + idx
-                        )
-
-                train_logs = self.train_step(
-                    anchor_image, positive_image, negative_image, metrics_list or []
-                )
-
-                for key, value in train_logs.items():
-                    accumulated_train_logs[key] = (
-                        accumulated_train_logs.get(key, 0) + value
-                    )
-
-                if callbacks:
-                    for callback in callbacks:
-                        for key, value in train_logs.items():
-                            callback.on_train_batch_end(
-                                {f"batch/{key}": value}, epoch * len(train_loader) + idx
-                            )
-
-            for key, value in accumulated_train_logs.items():
-                accumulated_train_logs[key] = value / len(train_loader)
-
-            # Compute and add training metrics to logs
-            if metrics_list is not None:
-                for metric in metrics_list:
-                    accumulated_train_logs[f"train/{metric!s}"] = metric.compute()
-            # Metrics are reset at the start of the next epoch or before validation
-
-            if callbacks:
-                for callback in callbacks:
-                    callback.on_train_epoch_end(accumulated_train_logs, epoch)
+            self.batch_train_loop(
+                train_loader=train_loader,
+                epoch=epoch,
+                epochs=epochs,
+                callbacks=callbacks,
+                metrics_list=metrics_list,
+            )
 
             # Validation loop over batches
-            accumulated_validation_logs: dict[str, Union[float, int]] = {}
-            if validation_loader is not None:
-                # Reset metrics for validation
-                if metrics_list is not None:
-                    for metric in metrics_list:
-                        metric.reset()
-                for idx, data in tqdm(
-                    enumerate(validation_loader),
-                    total=len(validation_loader),
-                    desc=f"Validation Epoch {epoch + 1}/{epochs}",
-                    leave=False,
-                ):
-                    if callbacks:
-                        for callback in callbacks:
-                            callback.on_validation_batch_start(
-                                {}, epoch * len(train_loader) + idx
-                            )
-
-                    anchor_image, positive_image, negative_image = data
-                    if self.train_transforms is not None:
-                        anchor_image = self.train_transforms(anchor_image)
-                        positive_image = self.train_transforms(positive_image)
-                        negative_image = self.train_transforms(negative_image)
-
-                    anchor_image = anchor_image.to(self.device)
-                    positive_image = positive_image.to(self.device)
-                    negative_image = negative_image.to(self.device)
-
-                    validation_logs = self.validation_step(
-                        anchor_image, positive_image, negative_image, metrics_list or []
-                    )
-
-                    for key, value in validation_logs.items():
-                        accumulated_validation_logs[key] = (
-                            accumulated_validation_logs.get(key, 0) + value
-                        )
-
-                    if callbacks:
-                        for callback in callbacks:
-                            for key, value in validation_logs.items():
-                                callback.on_validation_batch_end(
-                                    {f"batch/{key}": value},
-                                    epoch * len(train_loader) + idx,
-                                )
-
-                for key, value in accumulated_validation_logs.items():
-                    accumulated_validation_logs[key] = value / len(validation_loader)
-
-                # Compute and add validation metrics to logs
-                if metrics_list is not None:
-                    for metric in metrics_list:
-                        accumulated_validation_logs[f"validation/{metric!s}"] = (
-                            metric.compute()
-                        )
-                # Metrics will be reset at the start of the next training epoch loop
-
-            if callbacks:
-                for callback in callbacks:
-                    callback.on_validation_epoch_end(accumulated_validation_logs, epoch)
+            self.batch_validation_loop(
+                validation_loader=validation_loader,
+                epoch=epoch,
+                epochs=epochs,
+                callbacks=callbacks,
+                metrics_list=metrics_list,
+            )
 
             save_checkpoint(
                 model=self.model,
