2f53ca278763172fb604cb4d31c007fccb641827
soumik12345 <19soumik.rakshit96@gmail.com>
2025-06-25 11:13:16 +0530

    update: model tests

--- test/core/reid/dataset/test_base.py
+++ test/core/reid/dataset/test_base.py
@@ -5,6 +5,7 @@ import pytest
 
 from trackers.core.reid.dataset.base import IdentityDataset
 from trackers.core.reid.dataset.market_1501 import parse_market1501_dataset
+from trackers.core.reid.model import ReIDModel
 from trackers.utils.data_utils import unzip_file
 from trackers.utils.downloader import download_file
 
@@ -32,7 +33,23 @@ def test_identity_dataset(market_1501_dataset, dataset_split):
     dataset = IdentityDataset(parse_market1501_dataset(data_dir=dataset_path))
     if dataset_split == "bounding_box_train":
         assert len(dataset) == 12936
+        assert dataset.get_num_identities() == 751
     elif dataset_split == "bounding_box_test":
         assert len(dataset) == 15913
+        assert dataset.get_num_identities() == 751
     elif dataset_split == "query":
         assert len(dataset) == 3368
+        assert dataset.get_num_identities() == 750
+
+
+@pytest.mark.parametrize(
+    "dataset_split", ["bounding_box_train, bounding_box_test", "query"]
+)
+def test_reid_model_classification_head(market_1501_dataset, dataset_split):
+    dataset_path = os.path.join(market_1501_dataset, dataset_split)
+    dataset = IdentityDataset(parse_market1501_dataset(data_dir=dataset_path))
+    model = ReIDModel.from_timm("resnet50")
+    model.add_classification_head(
+        num_classes=dataset.get_num_identities(), freeze_backbone=True
+    )
+    assert model.backbone[-1].out_features == dataset.get_num_identities()
