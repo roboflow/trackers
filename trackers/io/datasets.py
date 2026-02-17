70c5a729d617de2807fe5f7d265198925d01ead7
Chandradithya <adithya.jvsc@gmail.com>
2026-02-17 12:08:27 +0530

    Added MOT Manifests

---
+++ trackers/io/datasets.py
@@ -0,0 +1,28 @@
+# trackers/datasets/manifest.py
+
+BASE_MOT17 = (
+    "https://storage.googleapis.com/com-roboflow-marketing/"
+    "trackers/datasets/mot17-v1"
+)
+
+DATASETS = {
+    "mot17": {
+        "description": "MOT17 benchmark Dataset.",
+        "splits": {
+            "train": {
+                "frames": f"{BASE_MOT17}/mot17-train-frames.zip",
+                "annotations": f"{BASE_MOT17}/mot17-train-annotations.zip",
+                "detections": f"{BASE_MOT17}/mot17-train-public-detections.zip",
+            },
+            "val": {
+                "frames": f"{BASE_MOT17}/mot17-val-frames.zip",
+                "annotations": f"{BASE_MOT17}/mot17-val-annotations.zip",
+                "detections": f"{BASE_MOT17}/mot17-val-public-detections.zip",
+            },
+            "test": {
+                "frames": f"{BASE_MOT17}/mot17-test-frames.zip",
+                "detections": f"{BASE_MOT17}/mot17-test-public-detections.zip",
+            },
+        },
+    }
+}
