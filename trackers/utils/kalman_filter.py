154c8307c9fea0c3eda2562ab33cabe3c515f926
Alex Bodner <yodabodner@gmail.com>
2026-02-19 12:04:55 -0300

    added headers to utils

--- trackers/utils/kalman_filter.py
+++ trackers/utils/kalman_filter.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------
+# Trackers
+# Copyright (c) 2026 Roboflow. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# -------
+
 from __future__ import annotations
 
 import numpy as np
