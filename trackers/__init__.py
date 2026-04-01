0c7a8af8716798f5f3856f356a57f67d663c976b
Alex Bodner <yodabodner@gmail.com>
2026-04-01 16:50:21 -0300

    Updated uv lock and pyproject.toml  for including torch and torchvision

--- trackers/__init__.py
+++ trackers/__init__.py
@@ -20,9 +20,10 @@ from trackers.motion.transformation import (
     IdentityTransformation,
 )
 from trackers.utils.converters import xcycsr_to_xyxy, xyxy_to_xcycsr
-from trackers.utils.iou import BaseIoU, CIoU, DIoU, GIoU, IoU
+from trackers.utils.iou import BIoU, BaseIoU, CIoU, DIoU, GIoU, IoU
 
 __all__ = [
+    "BIoU",
     "BaseIoU",
     "ByteTrackTracker",
     "CIoU",
